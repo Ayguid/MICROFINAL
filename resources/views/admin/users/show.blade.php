36a4c659984e84783074e3d4847bb7a0efd26a8c
Ayguid <guidoaimar@gmail.com>
2021-02-25 20:49:15 -0300

    users info display

--- resources/views/admin/users/show.blade.php
+++ resources/views/admin/users/show.blade.php
@@ -52,6 +52,38 @@
                     </div>
                   </div>
 
+                <div class="form-group row">
+                  <label for="telephone" class="col-md-4 col-form-label text-md-right">{{ __('Telephone') }}</label>
+
+                  <div class="col-md-6">
+                    <input disabled id="telephone" type="telephone" class="form-control" name="telephone" value="{{$data['user']->telephone}}">
+                    {{-- {{$data['user']->telephone}} --}}
+                      {{-- @error('tel')
+                        <span class="invalid-feedback" role="alert">
+                          <strong>{{ $message }}</strong>
+                        </span>
+                      @enderror --}}
+                    </div>
+                  </div>
+
+                <div class="form-group row">
+                  <label for="coutry" class="col-md-4 col-form-label text-md-right">{{ __('Country') }}</label>
+
+                  <div class="col-md-6">
+                    @if (!$data['user']->other_country_value)
+                      <input disabled id="telephone" type="telephone" class="form-control" name="telephone" value="{{App\Models440\Country::find($data['user']->country_id)->country_desc }}">
+                    @else
+                      <input disabled id="telephone" type="telephone" class="form-control" name="telephone" value="{{$data['user']->other_country_value }}">
+                    @endif
+                    {{-- {{$data['user']->telephone}} --}}
+                      {{-- @error('tel')
+                        <span class="invalid-feedback" role="alert">
+                          <strong>{{ $message }}</strong>
+                        </span>
+                      @enderror --}}
+                    </div>
+                  </div>
+
                   <div class="form-group row">
                     <label for="role" class="col-md-4 col-form-label text-md-right">{{ __('Role') }}</label>
 
